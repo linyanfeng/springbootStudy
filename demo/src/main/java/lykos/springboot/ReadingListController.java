cc3c4673c6fde5674101760a7f5fd72fe2f9e1a8
yanfenglin <yanfenglin@creditease.cn>
2017-07-04 11:46:07 +0800

    fix pom

--- demo/src/main/java/lykos/springboot/ReadingListController.java
+++ demo/src/main/java/lykos/springboot/ReadingListController.java
@@ -1,6 +1,5 @@
 package lykos.springboot;
 
-import cn.creditease.bdp.auth.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -31,20 +28,16 @@ public class ReadingListController {
         if (books == null) {
             return "readingList";
         }
-        logger.info("reader[{}]'s books:{}",reader,books);
+        logger.info("reader[{}]'s books:{}", reader, books);
         model.addAttribute("books", books);
         return "readingList";
     }
-    @RequestMapping(value = "/{reader}",method = RequestMethod.POST)
-    public String addToReadingList(@PathVariable("reader")String reader,Book book){
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
         return "redirect:/readingList/{reader}";
-    }
-    @RequestMapping(value = "/loginOut",method = RequestMethod.GET)
-    public String loginOut(@PathVariable("reader")String reader, Book book, HttpServletRequest request, HttpServletResponse response){
-        AuthService.logout(request,response);
-        return "";
     }
 }
 
