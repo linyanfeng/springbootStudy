63a8c18621ffd51102ae1651067d2da4e42d2f00
yanfenglin <yanfenglin@creditease.cn>
2017-09-03 14:31:19 +0800

    fix

--- demo/src/main/java/lykos/springboot/logoutController.java
+++ demo/src/main/java/lykos/springboot/logoutController.java
@@ -1,18 +1,14 @@
 package lykos.springboot;
 
-import cn.creditease.bdp.auth.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  * Created by lykos on 2017/6/22.
@@ -25,9 +21,8 @@ public class logoutController {
 
     private Logger logger = LoggerFactory.getLogger(logoutController.class);
 
-    @RequestMapping(value = "/me",method = RequestMethod.GET)
-    public String loginOut(HttpServletRequest request, HttpServletResponse response){
-        AuthService.logout(request,response);
+    @RequestMapping(value = "/me", method = RequestMethod.GET)
+    public String loginOut(HttpServletRequest request, HttpServletResponse response) {
         return "logout";
     }
 }
